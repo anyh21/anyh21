88635763c6a3fd569c191b9c994a382c961d3341
anyh21 <yeonghwan21@naver.com>
2024-11-26 14:21:59 +0900

    Delete LoginFilter.java

--- LoginFilter.java
+++
@@ -1,9 +0,0 @@
-protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws Exception
-{
-  if (userSession.getAttribute("CSRF_TOKEN") == null)
-  {
-    userSession.setAttribute("CSRF_TOKEN", UUID.randomUUID().toString());
-  }
-  String sessionCsrf = userSession.getAttribute("CSRF_TOKEN").toString();
-  res.setHeader("X-CSRF-TOKEN", sessionCsrf);
-}  
