4e738c5eede639f2b5863f569bf4b07e6fa7c292
anyh21 <yeonghwan21@naver.com>
2024-11-26 14:22:29 +0900

    Delete cstmForm.js

--- cstmForm.js
+++
@@ -1,6 +0,0 @@
-this.form = $('<form>').appendTo('body').get(0);
-
-var httpRequest = new XMLHttpRequest();
-httpRequest.open('GET', document.location, false);
-httpRequest.send(null);
-$('<input type="hidden" name="_csrf">').val(httpRequest.getResponseHeader('X-CSRF-TOKEN')).appendTo(this.form);
